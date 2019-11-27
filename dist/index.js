3c9ebe44880f9e235fcd484583d4a3e8af82aac9
Extra <extrahash@protonmail.com>
2019-11-27 16:18:54 -0700

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('This is the main entry point of the program.');
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log('This is the main entry point of the program.');
+
+const test = {
+  value: 'test',
+};
+
+console.log(test.value);
+
+let test2 = 0;
+
+let test3 = 5;
