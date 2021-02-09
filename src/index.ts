b443ea766251a1796ed2e09cef653ab914a18271
ExtraHash@protonmail.com <ExtraHash@protonmail.com>
2021-02-09 23:31:15 +0000

    bump dependencies and switch logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import log from "electron-log";
+import log from "loglevel";
 import { loadEnv } from "./utils/loadEnv";
 
 // load the environment variables
 loadEnv();
 
 function main() {
+    log.setLevel("debug");
+
     log.info("This is the main entry point of the program.");
     const test = {
         value: "test",
