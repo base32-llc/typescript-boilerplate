0146552fbfe0a50daeeeb340f82f93775b1c1f0e
ExtraHash <extrahash@protonmail.com>
2022-03-23 12:14:18 -0600

    update loadenv

--- src/utils/loadEnv.ts
+++ src/utils/loadEnv.ts
@@ -7,12 +7,19 @@ import { config } from "dotenv";
 export function loadEnv(): void {
     config();
     const requiredEnvVars: string[] = [];
+
+    const notFound: string[] = [];
+
     for (const required of requiredEnvVars) {
         if (process.env[required] === undefined) {
-            console.warn(
-                `Required environment variable '${required}' is not set. Please consult the README.`
-            );
-            process.exit(1);
+            notFound.push(required);
         }
+    }
+
+    if (notFound.length > 0) {
+        console.warn(
+            `Required environment variables '${notFound.join(", ")}' are not set. Please consult the README.`
+        );
+        process.exit(1);
     }
 }
